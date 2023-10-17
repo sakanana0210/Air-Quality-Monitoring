eb0749e06e9cfded4701998810824cde5586460c
Zoey Lu <luchin601@gmail.com>
2023-10-18 03:18:49 +0800

    Style and Robot feature
    -Add send current site data to discord feature
    -Add scroll effect and link to each section
    -Add aqi animation html frame

--- script/main.js
+++ script/main.js
@@ -4,4 +4,44 @@ Array.from(result).forEach((indexText)=>{
     indexText.innerHTML = 35
 })
 document.getElementById("aqi_description").innerText = "普通"
+document.getElementById("aqc_time").innerText = "2023/10/16 17:00"
+document.getElementById("legend_name").innerHTML = "NO<sub>2</sub>"
 ///////////////////////////////////////////////////
+const mapLink = document.getElementById("map_link")
+const siteLink = document.getElementById("site_link")
+const aqcGraphLink = document.getElementById("aqc_graph_link")
+const trendLink = document.getElementById("trend_link")
+const headerTitle = document.getElementById("header_title")
+
+const scrollToTarget = (targetIdName) => {
+    const target = document.getElementById(targetIdName)
+    const headerHeight = document.getElementById("header").offsetHeight;
+    const targetPosition = target.offsetTop - headerHeight;
+    window.scrollTo({
+        top: targetPosition,
+        behavior: "smooth"
+    })
+}
+
+mapLink.addEventListener("click",()=>{
+    scrollToTarget("first_divider")
+})
+
+siteLink.addEventListener("click",()=>{
+    scrollToTarget("second_divider")
+})
+
+aqcGraphLink.addEventListener("click",()=>{
+    scrollToTarget("third_divider")
+})
+
+trendLink.addEventListener("click",()=>{
+    scrollToTarget("forth_divider")
+})
+
+headerTitle.addEventListener("click",()=>{
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+})
