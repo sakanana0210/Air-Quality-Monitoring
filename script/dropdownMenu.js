43ad395f2f71e8e1dd9aebf5140875dbf0257a83
shaurair <shaurair@gmail.com>
2023-10-18 22:26:48 +0800

    Modify: revise map actions of search button

--- script/dropdownMenu.js
+++ script/dropdownMenu.js
@@ -1,3 +1,5 @@
+import { getNewMap } from './map.js';
+
 let url = "https://data.moenv.gov.tw/api/v2/aqx_p_488?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=datacreationdate%20desc&format=JSON";
 let records;
 let dataTest;
@@ -122,9 +124,7 @@ function setTimeList() {
 
 function setSearchActions() {
 	let settingTime = timeSelectionElement.value;
-	fetchForMap().then(mapDatas => {
-		createCircle(mapDatas, settingTime);
-	})
+	getNewMap(settingTime);
 }
 
 // Change select actions
